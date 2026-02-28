85bac5763845680e854f940937c3aabb2aa4b02e
Apacapp01 <apacapp402@gmail.com>
2026-02-28 10:38:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,12 @@ import base64
 import pickle
 import numpy as np
 import mediapipe as mp
-import tensorflow as tf
+import tflite_runtime.interpreter as tflite
 
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 from PIL import Image
-
-# ---------------- HARD LIMIT TF (RENDER SAFE) ----------------
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-os.environ["OMP_NUM_THREADS"] = "1"
-os.environ["TF_NUM_INTRAOP_THREADS"] = "1"
-os.environ["TF_NUM_INTEROP_THREADS"] = "1"
-
-tf.config.threading.set_intra_op_parallelism_threads(1)
-tf.config.threading.set_inter_op_parallelism_threads(1)
 
 # ---------------- APP SETUP ----------------
 app = Flask(__name__)
@@ -33,12 +24,12 @@ app.config["MAX_CONTENT_LENGTH"] = 5 * 1024 * 1024
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
 
 # ---------------- GLOBAL CACHE ----------------
-models = {}       # lazy-loaded models
+models = {}       # lazy-loaded TFLite interpreters
 scaler = None
 labels = None
 hands = None
 
-# ---------------- LAZY LOAD RESOURCES ----------------
+# ---------------- LAZY LOAD COMMON RESOURCES ----------------
 def load_common_resources():
     global scaler, labels, hands
 
@@ -57,21 +48,22 @@ def load_common_resources():
             min_detection_confidence=0.5
         )
 
+# ---------------- LOAD TFLITE MODEL ----------------
 def load_model(model_type):
     if model_type in models:
         return models[model_type]
 
-    from tensorflow.keras.models import load_model
-
     model_paths = {
-        "live": "models/final_model.keras",
-        "letter": "models/L_model.h5",
-        "number": "models/N_model.h5"
+        "live": "models/final_model.tflite",
+        "letter": "models/L_model.tflite",
+        "number": "models/N_model.tflite"
     }
 
-    model = load_model(model_paths[model_type], compile=False)
-    models[model_type] = model
-    return model
+    interpreter = tflite.Interpreter(model_path=model_paths[model_type])
+    interpreter.allocate_tensors()
+
+    models[model_type] = interpreter
+    return interpreter
 
 # ---------------- UTILS ----------------
 def allowed_file(filename):
@@ -92,7 +84,17 @@ def extract_keypoints(image_np):
                 keypoints[idx:idx+3] = (lm.x, lm.y, lm.z)
                 idx += 3
 
-    return scaler.transform(keypoints.reshape(1, -1))
+    return scaler.transform(keypoints.reshape(1, -1)).astype(np.float32)
+
+def predict_tflite(interpreter, input_data):
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    interpreter.set_tensor(input_details[0]["index"], input_data)
+    interpreter.invoke()
+
+    output = interpreter.get_tensor(output_details[0]["index"])
+    return output
 
 # ---------------- ROUTES ----------------
 @app.route("/")
@@ -104,7 +106,7 @@ def upload_image():
     load_common_resources()
 
     file = request.files.get("file")
-    model_type = request.form.get("type", "live")  # live | letter | number
+    model_type = request.form.get("type", "live")
 
     if not file or not allowed_file(file.filename):
         return jsonify({"error": "Invalid file"}), 400
@@ -118,9 +120,9 @@ def upload_image():
         img_np = np.array(img)
 
         keypoints = extract_keypoints(img_np)
-        model = load_model(model_type)
+        interpreter = load_model(model_type)
 
-        pred = model.predict(keypoints, verbose=0)
+        pred = predict_tflite(interpreter, keypoints)
         label = labels[int(np.argmax(pred))]
 
         return jsonify({
@@ -135,7 +137,7 @@ def upload_image():
 @app.route("/process_frame", methods=["POST"])
 def process_frame():
     load_common_resources()
-    model = load_model("live")
+    interpreter = load_model("live")
 
     try:
         data = request.json["image"].split(",")[1]
@@ -145,7 +147,7 @@ def process_frame():
         )
 
         keypoints = extract_keypoints(frame)
-        pred = model.predict(keypoints, verbose=0)
+        pred = predict_tflite(interpreter, keypoints)
 
         return jsonify({
             "has_hands": True,
@@ -159,5 +161,3 @@ def process_frame():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
-
